89eb1f30977661f715d8164f86c48560dc1a9863
kavinprasanth <kavinprasanth7274@gmail.com>
2024-05-25 16:44:05 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:latest as build
+
+# build app
+WORKDIR /src
+COPY package.json package-lock.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+# serve to nginx
+FROM nginx:latest
+WORKDIR /usr/share/nginx/html
+RUN rm -rf *
+COPY --from=build /src/build .
+EXPOSE 80
+ENTRYPOINT [ "nginx", "-g", "dsemon off;" ]
+
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+import './App.css';
+import List from './components/List';
+
+function App() {
+
+  const [input, setInput] = useState([]);
+  const [listItem, setListItem] = useState([]);
+  const [mode, setMode] = useState("add");
+  const [id, setId] = useState("");
+
+  let BASE_URI = process.env.REACT_APP_NODE_ENV == "development" ? process.env.REACT_APP_LOCAL_BASE_URI : process.env.REACT_APP_SERVER_BASE_URI;
+
+
+  const handleList = async () => {
+    if (input != '' && input != null && input != undefined && mode == "add") {
+      const Input = { todos: input }
+      axios.post(BASE_URI + '/create-todo', Input).then((data) => {
+        console.log(data)
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+    else if (mode == "edit") {
+      const Input = { todos: input }
+      axios.put(BASE_URI + `/update-todo/${id}`, Input).then(({ data }) => {
+        console.log(data)
+        setMode("add")
+        setInput("")
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  }
+
+  useEffect(() => {
+    axios.get(BASE_URI + '/get-todo').then(({ data }) => {
+      setListItem(data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+
+
+
+  return (
+    <main className="todo_list">
+      <div className="main_holder">
+        <h1>CRUD Operations</h1>
+        <div className="input_holder">
+          <input className="input" value={input} onChange={(e) => setInput(e.target.value)}></input>
+          <button onClick={handleList}>{mode == "add" ? "Add" : "Update"}</button>
+        </div>
+        <div className='list_holder'>
+          <h2>List</h2>
+          <List listItem={listItem} setInput={setInput} setMode={setMode} setId={setId} />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
